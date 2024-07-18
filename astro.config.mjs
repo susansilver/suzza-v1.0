2b859735425a5897a4b91cd45cc5ca3df4e002b4
suzza <suzza@susansilver.net>
2024-07-18 06:20:50 -0700

    add integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
+import favicons from "astro-favicons";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  site: 'https://suzza.net',
+  integrations: [mdx(), tailwind(), favicons({
+    masterPicture: "public/logo-white.jpg",
+    emitAssets: true,
+    faviconsDarkMode: false,
+  })]
 });
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Suzza Silver';
+export const SITE_DESCRIPTION = 'Helping Early Career Job Seekers Find Networking Opportunities Through Blogging';

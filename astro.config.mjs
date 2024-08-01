b92cad1dbe434afe646c82d82a239de5b21615bb
suzza <suzza@susansilver.net>
2024-08-01 12:38:49 -0700

    add integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,35 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import favicons from "astro-favicons";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import embeds from "astro-embed/integration";
+import betterImageService from "astro-better-image-service";
+import compressor from "astro-compressor";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://suzza.net',
-  integrations: [mdx(), tailwind(), favicons({
-    masterPicture: "public/logo-white.jpg",
-    emitAssets: true,
-    faviconsDarkMode: false
-  }), sitemap()]
+  site: "https://suzza.net",
+  integrations: [
+    embeds(),
+    mdx(),
+    tailwind(),
+    favicons({
+      masterPicture: "public/logo-white.jpg",
+      emitAssets: true,
+      faviconsDarkMode: false,
+    }),
+    sitemap(),
+    betterImageService(),
+    (await import("@playform/compress")).default({
+      CSS: true,
+      HTML: true,
+      Image: false,
+      JavaScript: true,
+      SVG: false,
+    }),
+    compressor(),
+  ],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Suzza Silver';
-export const SITE_DESCRIPTION = 'Helping Early Career Job Seekers Find Networking Opportunities Through Blogging';
+export const SITE_TITLE = "Suzza Silver";
+export const SITE_DESCRIPTION =
+  "A community of writers who want to get more traffic to their blogs.";

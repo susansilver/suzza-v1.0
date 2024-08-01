0d2d3425c03159a94996100583f1ef6a47fe75f3
suzza <suzza@susansilver.net>
2024-08-01 12:09:19 -0700

    add rss feed and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import mdx from "@astrojs/mdx";
 import favicons from "astro-favicons";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://suzza.net',
   integrations: [mdx(), tailwind(), favicons({
     masterPicture: "public/logo-white.jpg",
     emitAssets: true,
-    faviconsDarkMode: false,
-  })]
+    faviconsDarkMode: false
+  }), sitemap()]
 });
